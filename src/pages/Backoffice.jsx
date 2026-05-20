f639d2be12e924ac9c1d9df871f730e3a057b916
AlexisGerv <gervaudalexis@gmail.com>
2026-05-20 14:46:07 +0200

    Ajout de film et d'édition

--- src/pages/Backoffice.jsx
+++ src/pages/Backoffice.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";   
-import addMovie from "../components/addMovie";  
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddMovie from "../Components/addMovie";
 
 export default function Backoffice() {
-  return (<h1>Backoffice</h1>);
+  return (
+    <div>
+      <h1>Backoffice</h1>
+      <AddMovie />
+    </div>
+  );
 }
