929e5d575c270ff5b71bdb434736f431e47e57b3
zeyy-gif <arslanzeynep007@gmail.com>
2026-05-20 16:26:41 +0200

    test

--- src/pages/PageFilm.jsx
+++ src/pages/PageFilm.jsx
@@ -1,1 +1,64 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export default function FilmProgrammed() {
+  const [films, setFilms] = useState([]);
+  const [id, setId] = useState('');
+
+
+  useEffect(() => {
+    fetch(`${API_URL}/films/get`+id)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Erreur lors du chargement des films");
+        }
+
+        return res.json();
+      })
+      .then((data) => {
+        setFilms(data);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="title">CinéClub</h1>
+          <div className="card" key={film.id}>
+            <img src={`${API_URL}/${film.url_image}`} alt={film.name} />
+            <div className="card-content">
+              <h2>{film.name}</h2>
+
+              {film.author && (
+                <p>
+                  <strong>Réalisateur :</strong> {film.author}
+                </p>
+              )}
+
+              {film.film_genre && (
+                <p>
+                  <strong>Genre :</strong> {film.film_genre}
+                </p>
+              )}
+
+
+              {film.projection_date && (
+                <p>
+                  <strong>Projection :</strong>{" "}
+                  {new Date(film.projection_date).toLocaleDateString()}
+                </p>
+              )}
+
+              {film.cinema && (
+                <p>
+                  <strong>Cinéma :</strong> {film.cinema}
+                </p>
+              )}
+
+              
+           
+            </div>
+          </div>
+      </div>
+  );
+}
