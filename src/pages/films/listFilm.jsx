a719e58070dfa3963201433ffdb64a5e60eefb51
zeyy-gif <arslanzeynep007@gmail.com>
2026-05-20 14:51:34 +0200

    list film

--- src/pages/films/listFilm.jsx
+++ src/pages/films/listFilm.jsx
@@ -2,7 +2,7 @@ import { useFilms } from "./useFilms";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-export default function SmallFilm() {
+export default function ListFilm() {
   const { films, loading, error } = useFilms();
 
   if (loading) {
