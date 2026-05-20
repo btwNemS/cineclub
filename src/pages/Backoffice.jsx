747b205a9e97a93fd4f98ef722d5af7aacc04c71
AlexisGerv <gervaudalexis@gmail.com>
2026-05-20 15:08:25 +0200

    test edit

--- src/pages/Backoffice.jsx
+++ src/pages/Backoffice.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Typography, Box } from '@mui/material';
 import AddMovie from "../Components/addMovie";
 import EditMovie from "../Components/editMovie";
 
 export default function Backoffice() {
   return (
-    <div>
-      <h1>Backoffice</h1>
-        <AddMovie />
-        <EditMovie />
-    </div>
+    <Box>
+      <Typography variant="h1">Backoffice</Typography>
+      <AddMovie />
+      <EditMovie />
+    </Box>
   );
 }
