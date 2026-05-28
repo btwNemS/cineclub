6f2d6a46863ff160da941959b6c05d37bb6a2659
zeyy-gif <arslanzeynep007@gmail.com>
2026-05-28 17:07:44 +0200

    liste inscrits en cours

--- src/pages/InscriptionSeance.jsx
+++ src/pages/InscriptionSeance.jsx
@@ -3,7 +3,6 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 import { useTheme } from "@mui/material/styles";
 import { useAuth } from "../Authentification";
 
@@ -12,9 +11,10 @@ export default function InscriptionSeance() {
   const { id } = useParams();
   const { user } = useAuth();
   const theme = useTheme();
-  const [film, setFilm] = useState(null)
-  ;
-
+  const [inscrits, setInscrits] = useState([]);
+  const [isSuscribed, setisSuscribed] = useState(false);
+   const [liste, setListe] = useState(false);
+ 
   useEffect(() => {
     fetch(`${API_URL}/registrations/getRegistrationsByFilmId/${id}`)
       .then((res) => {
@@ -25,15 +25,31 @@ export default function InscriptionSeance() {
         return res.json();
       })
       .then((data) => {
-        setFilm(data);
+        setInscrits(data);
       });
   }, [id]);
 
-    return  <Button
+  function handleHover() {
+  	setListe(true);
+  }
+  function handleClick() {
+  	console.log("ça marche aussi");
+  }
+
+    return  (
+        <div>
+    <Button onMouseEnter={handleHover} onClick={handleClick}
               variant="outlined"
               color="secondary"
               target="_blank"
             >
               S'inscrire
             </Button>;
+         
+             {inscrits.map((inscrit) => (
+                  <li>{inscrit.pseudo}</li>   
+          ))}
+          </div>
+    )
+    
 }
