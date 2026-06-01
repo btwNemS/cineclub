e15460933172453ade8761111e10d216eb939872
Tristan Goujan <tgoujan@gmail.com>
2026-06-01 16:42:16 +0200

    vérification role pour back office

--- src/pages/Backoffice.jsx
+++ src/pages/Backoffice.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Typography, Box } from '@mui/material';
-import AddMovie from "../Components/addMovie";
+import AddMovie from "../Components/AddMovie";
+import { useAuth } from "../Authentification";
+import AuthModal from "../AuthModal";
+
+
 
 export default function Backoffice() {
-  return (
-    <Box>
-      <AddMovie />
-    </Box>
-  );
+    const { user } = useAuth();
+    return (
+      <>
+        {user?.role !== "ADMIN" && (
+          <Box sx={{ p: 4 ,textAlign: "center"}}>
+            <Typography variant="h4" color="error">
+              Accès restreint
+            </Typography>
+            <Typography variant="body1">
+              Vous devez être administrateur pour accéder à cette page.
+            </Typography>
+          </Box>
+        )}
+        {user?.role === "ADMIN" && (
+          <Box>
+            <AddMovie />
+          </Box>
+        )}
+      </>
+    );
 }
