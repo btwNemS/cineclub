9b9aa933dd34609e2c3e3cc8b30dd31134267091
Nafaur21000 <nael.faure@iut-dijon.u-bourgogne.fr>
2026-06-01 16:27:38 +0200

    depuis la page des films suggérés, on a la possibilité de voter et regarder la moyenne des notes des films

--- src/pages/FilmSuggested.jsx
+++ src/pages/FilmSuggested.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import CardMovie from "../Components/cardMovie";
 
 const API_URL = import.meta.env.VITE_API_URL;
@@ -7,22 +6,21 @@ const API_URL = import.meta.env.VITE_API_URL;
 export default function FilmSuggested() {
   const [films, setFilms] = useState([]);
 
-  useEffect(() => {
+  const refreshFilms = () => {
     fetch(`${API_URL}/films/getAll`)
       .then((res) => {
-        if (!res.ok) {
-          throw new Error("Erreur lors du chargement des films");
-        }
-
+        if (!res.ok) throw new Error("Erreur de chargement");
         return res.json();
       })
-      .then((data) => {
-        setFilms(data);
-      });
+      .then((data) => setFilms(data))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    refreshFilms();
   }, []);
 
   return (
-
     <div className="container">
       <h1 className="title">Films suggérés</h1>
 
@@ -30,9 +28,12 @@ export default function FilmSuggested() {
         {films
           .filter((film) => film.status === "suggested")
           .map((film) => (
-            <Link to={`/film/${film.id}`} key={film.id}>
-              <CardMovie film={film} />
-            </Link>
+            // Plus de composant <Link> encombrant ici ! C'est CardMovie qui gère ses zones.
+            <CardMovie 
+              key={film.id} 
+              film={film} 
+              onVoteSuccess={refreshFilms} 
+            />
           ))}
       </div>
     </div>
