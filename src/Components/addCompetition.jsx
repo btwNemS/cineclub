85344feaf35d5ca13e823ca3ee1f01a1d5aefdd7
Nafaur21000 <nael.faure@iut-dijon.u-bourgogne.fr>
2026-06-02 09:08:10 +0200

    pareil mb pour les 2

--- src/Components/addCompetition.jsx
+++ src/Components/addCompetition.jsx
@@ -1,8 +1,23 @@
-import 
+import { useState } from 'react';
+import { Typography, Box, Button, Stack } from '@mui/material';
 
 export default function AddCompetition() {
   return (
-    
-
+    <Box sx={{ p: 4 }}>
+      <Typography variant="h4" mb={2}>
+        Créer ou Modifier une Compétition
+      </Typography>
+      <Stack spacing={2}>
+        {/* Formulaire pour ajouter ou modifier une compétition */}
+        <Item>+</Item>
+        <Item>+</Item>
+        <Item>+</Item>
+        <Item>+</Item>
+        <Item>+</Item>
+        <Button variant="contained" color="primary">
+          Enregistrer
+        </Button>
+      </Stack>
+    </Box>
   );
 }
